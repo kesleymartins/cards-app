a6d2cb825b4dc7a47bead67c568d811b13681f58
kesleymartins <kesley.martins@vivaldi.net>
2023-02-26 14:18:06 -0300

    create collections crud

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,45 @@
+class CollectionsController < ApplicationController
+  before_action :set_collection, only: %i[ edit update destroy ]
+
+  def index
+    @collections = Collection.all
+  end
+
+  def new
+    @collection = Collection.new
+  end
+
+  def edit; end
+
+  def create
+    @collection = Collection.new(collection_params)
+
+    if @collection.save
+      redirect_to collections_path, notice: "Coleção criada com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @collection.update(collection_params)
+      redirect_to collections_path, notice: "Collection atualizada com sucesso!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @collection.destroy
+    redirect_to collections_url, notice: "Collection removida com sucesso!."
+  end
+
+  private
+    def set_collection
+      @collection = Collection.find(params[:id])
+    end
+
+    def collection_params
+      params.require(:collection).permit(:title, :description)
+    end
+end
