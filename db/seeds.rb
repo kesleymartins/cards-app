8d98e0967d236204f939edb6ff081c5189783baf
kesleymartins <kesley.martins@vivaldi.net>
2023-02-27 10:32:03 -0300

    setup seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+50.times do
+    collection = Collection.create!({
+        title: Faker::Games::WorldOfWarcraft.hero,
+        description: Faker::Games::WorldOfWarcraft.quote
+    })
+
+    100.times do
+        Card.create!({
+            question: Faker::Books::Dune.quote,
+            awser: Faker::Books::Dune.character,
+            collection: collection
+        })
+    end
+end
