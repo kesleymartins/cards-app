f6546f80bf0d9f9a3b5e332009bf245c04625f4a
kesleymartins <kesley.martins@vivaldi.net>
2023-02-28 10:02:58 -0300

    fix identation 4 -> 2

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -1,8 +1,8 @@
 class Cycle < ApplicationRecord
-    enum status: { created: 0, running: 1, closed: 2 }
+  enum status: { created: 0, running: 1, closed: 2 }
 
-    validates :size, presence: true, length: { minimum: 1 }
+  validates :size, presence: true, length: { minimum: 1 }
 
-    belongs_to :collection
-    has_many :cycle_cards, dependent: :destroy
+  belongs_to :collection
+  has_many :cycle_cards, dependent: :destroy
 end
