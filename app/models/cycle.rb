c34ad4f1da0bba3db0114e6bbc9969f41859eafb
kesleymartins <kesley.martins@vivaldi.net>
2023-02-28 12:44:09 -0300

    create method to setup cycle cards

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -5,4 +5,21 @@ class Cycle < ApplicationRecord
 
   belongs_to :collection
   has_many :cycle_cards, dependent: :destroy
+
+  after_create :chose_cards
+
+  private
+
+  def chose_cards
+    avaliable_cards = self.collection.cards
+    selected_cards = avaliable_cards.shuffle.slice(1..self.size)
+
+    selected_cards.each do |card|
+      CycleCard.create!({
+        status: CycleCard.statuses[:waiting],
+        card: card,
+        cycle: self
+      })
+    end
+  end
 end
