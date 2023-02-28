503b84989db64b26afa61ea2be74e6b1e28b2ffc
kesleymartins <kesley.martins@vivaldi.net>
2023-02-28 12:43:50 -0300

    create method to find collection current cycle

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,11 @@
 class Collection < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
 
-    has_many :cards, dependent: :destroy
-    has_many :cycles, dependent: :destroy
+  has_many :cards, dependent: :destroy
+  has_many :cycles, dependent: :destroy
+
+  def current_cycle
+    self.cycles.find_by(status: Cycle.statuses[:created])
+  end
 end
