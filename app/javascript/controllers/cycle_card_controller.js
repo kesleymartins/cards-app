4ef34d25973354c380d64c2a2bee5cb7b4327de9
kesleymartins <kesley.martins@vivaldi.net>
2023-03-01 17:06:18 -0300

    some script improvements

--- app/javascript/controllers/cycle_card_controller.js
+++ app/javascript/controllers/cycle_card_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class CycleCardController extends Controller {
+    static targets = ["button", "answerBlock"]
+    
+    connect() {
+        this.buttonTarget.addEventListener('click', () => {
+            this.showAnswer()
+            this.removeShowAnswerButton()
+        })
+    }
+    
     showAnswer() {
-        this.element.querySelector("#answer-block").classList.remove('is-hidden')
+        this.answerBlockTarget.classList.remove('is-hidden')
+    }
+
+    removeShowAnswerButton() {
+        this.buttonTarget.remove();
     }
 }
