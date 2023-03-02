41b1b2357297998226128cd87af036c5ba87aa9d
kesleymartins <kesley.martins@vivaldi.net>
2023-03-02 14:11:13 -0300

    add logic awnser when a cycle card is answered

--- app/controllers/cycle_cards_controller.rb
+++ app/controllers/cycle_cards_controller.rb
@@ -1,5 +1,23 @@
 class CycleCardsController < ApplicationController
-  def show
+  def update
     @cycle_card = CycleCard.find(params[:id])
+    @cycle_card.answered!
+
+    @cycle_card.cycle.increase_progress!
+
+    if @cycle_card.cycle.has_finished?
+      @cycle_card.cycle.destroy
+      redirect_to root_path
+    else
+      render(
+        turbo_stream: turbo_stream.update(
+          :cycle_card, 
+          partial: 'cycle_cards/show', 
+          locals: { 
+            cycle_card: @cycle_card.cycle.next_cycle_card, 
+          }
+        )
+      )
+    end
   end
 end
