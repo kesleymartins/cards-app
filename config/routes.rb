c826fc62af2cc9309c5c299e3c46e6e5757ea266
kesleymartins <kesley.martins@vivaldi.net>
2023-03-02 14:11:25 -0300

    setup new routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resources :cards, except: %i[ show ]
 
     resources :cycles, only: %i[ new create show ], shallow: true do
-      resources :cycle_cards, only: %i[ show ], shallow: true
+      resources :cycle_cards, only: %i[ update ], shallow: true
     end
   end
+
+  get 'cycles/:id/next-card', to: 'cycles#next_card', as: :cycle_next_card
 end
