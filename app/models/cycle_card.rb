166b2b4cf67236a4e84d3d33a78386d1a0b3d17c
kesleymartins <kesley.martins@vivaldi.net>
2023-03-03 18:20:38 -0300

    create methods to mark cycle_card as answered and score card on update

--- app/models/cycle_card.rb
+++ app/models/cycle_card.rb
@@ -1,4 +1,6 @@
 class CycleCard < ApplicationRecord
+  attribute :card_answer, :string
+
   enum status: { waiting: 0, answered: 1 }
 
   validates :status, presence: true
@@ -6,4 +8,27 @@ class CycleCard < ApplicationRecord
 
   belongs_to :card
   belongs_to :cycle
+
+  before_update :mark_as_answered
+  after_update :score_card
+  
+  private
+
+  def mark_as_answered
+    self.status = CycleCard.statuses[:answered] 
+  end
+
+  def score_card
+    score = Score.find_by(card: self.card)
+
+    unless score
+      score = Score.create!({card: self.card})
+    end
+
+    if self.card_answer == 'error'
+      score.decrement!
+    elsif self.card_answer == 'success' 
+      score.increment!
+    end
+  end
 end
