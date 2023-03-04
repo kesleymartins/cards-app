00c6df8298ca359255272ae0a4703b6644b440ca
kesleymartins <kesley.martins@vivaldi.net>
2023-03-04 15:54:59 -0300

    set diferent root path if user is authenticated or not

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'collections#index'
+    end
+  
+    unauthenticated do
+      root 'pages#home', as: :unauthenticated_root
+    end
+  end
 
   resources :collections, except: %i[ show ] do
     resources :cards, except: %i[ show ]
@@ -10,6 +20,4 @@ Rails.application.routes.draw do
   end
 
   get 'cycles/:id/next-card', to: 'cycles#next_card', as: :cycle_next_card
-
-  devise_for :users
 end
