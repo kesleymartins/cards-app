a2b15ced0e4d35cef5e24142313ff2bbec5ca44a
kesleymartins <kesley.martins@vivaldi.net>
2023-03-04 14:49:54 -0300

    create scope to filter by user

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
+  scope :by_user, -> (user) { where(user: user) }  
+
   def self.latest
     order(created_at: :desc)
   end
