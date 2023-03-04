f63c710d6aaccd5a11986f5230cd21051787814f
kesleymartins <kesley.martins@vivaldi.net>
2023-03-04 14:52:56 -0300

    set latest order as scope

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,8 +2,5 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   scope :by_user, -> (user) { where(user: user) }  
-
-  def self.latest
-    order(created_at: :desc)
-  end
+  scope :latest, -> { order(created_at: :desc)}
 end
