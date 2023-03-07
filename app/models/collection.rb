79aaa2104f0d64b9fb3010cecbb58fdb29621056
kesleymartins <kesley.martins@vivaldi.net>
2023-03-06 22:52:26 -0300

    update to new cycle logic

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -6,4 +6,15 @@ class Collection < ApplicationRecord
 
   has_many :cards, dependent: :destroy
   has_many :executions, dependent: :destroy
+
+  def can_execute?
+    not self.cards.empty?
+  end
+
+  def execution
+    Execution.find_by(
+      collection: self,
+      user: self.user
+    )
+  end
 end
