69910b5dc7dcfb7d9e10f28415ecb123b3437db1
kesleymartins <kesley.martins@vivaldi.net>
2023-03-07 00:51:38 -0300

    update status to complete when is last card

--- app/services/CycleLinker.rb
+++ app/services/CycleLinker.rb
@@ -2,6 +2,11 @@ class CycleLinker
   def initialize(cycle:)
     @cycle = cycle
     @execution = cycle.execution
+    @status = @execution.status
+
+    if @cycle.is_last?
+      @status = ExecutionStatus::CLOSED 
+    end
   end
 
   def call(type:)
@@ -13,7 +18,8 @@ class CycleLinker
   def success
     @execution.update!({
       score: @execution.score + 1,
-      cycle_id: @cycle.next_id 
+      cycle_id: @cycle.next_id,
+      status: @status
     })
 
     @cycle.destroy
@@ -22,7 +28,8 @@ class CycleLinker
   def error
     @execution.update!({
       score: @execution.score - 1,
-      cycle_id: @cycle.next_id 
+      cycle_id: @cycle.next_id,
+      status: @status
     })
 
     @cycle.destroy
