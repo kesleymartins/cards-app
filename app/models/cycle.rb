9ce2f83ca5355410e3fe6c443793ad4d117ea323
kesleymartins <kesley.martins@vivaldi.net>
2023-03-08 00:19:40 -0300

    fix association

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -5,7 +5,6 @@ class Cycle < ApplicationRecord
   belongs_to :card
 
   has_one :cycle, class_name: 'Cycle', foreign_key: 'next_id'
-  belongs_to :manager, class_name: 'Cycle', optional: true
 
   def is_last?
     self.next_id.nil?
