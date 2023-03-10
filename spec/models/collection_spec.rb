3be201874b21e22772440de054f09e3bde9e569c
kesleymartins <kesley.martins@vivaldi.net>
2023-03-09 21:12:57 -0300

    add privacy validation to spec

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Collection, type: :model do
   context "validations" do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:privacy) }
   end
 
   context "associations" do
