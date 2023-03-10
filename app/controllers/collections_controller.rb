efa71039d62b0091f64e4fb22c362c32d0875fd1
kesleymartins <kesley.martins@vivaldi.net>
2023-03-09 21:09:34 -0300

    allow privacy on collection strong params

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -55,6 +55,6 @@ class CollectionsController < ApplicationController
     end
 
     def collection_params
-      params.require(:collection).permit(:title, :description)
+      params.require(:collection).permit(:title, :description, :privacy)
     end
 end
