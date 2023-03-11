35cd4472e189986639abb9c40c60d729585b76dc
kesleymartins <kesley.martins@vivaldi.net>
2023-03-11 15:25:21 -0300

    implement card policy

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,22 +1,28 @@
 class CardsController < ApplicationController
   before_action :authenticate_user!
-
   before_action :set_collection
   before_action :set_card, only: %i[ edit update destroy ]
   
   def index
     @pagy, @cards = pagy(Card.where(collection: @collection).latest)
+    authorize @collection, :list_cards?
   end
 
   def new
     @card = Card.new
+
+    authorize @card
   end
   
-  def edit; end
+  def edit
+    authorize @card
+  end
 
   def create
     @card = Card.new(card_params)
     @card.collection = @collection
+
+    authorize @collection, :create_card?
 
     if @card.save
       redirect_to collection_cards_path(@collection), notice: 'Carta criada com sucesso!'
@@ -32,6 +38,8 @@ class CardsController < ApplicationController
   end
 
   def update
+    authorize @card
+    
     if @card.update(card_params)
       redirect_to collection_cards_path(@collection), notice: 'Carta atualizada com sucesso!'
     else
@@ -46,6 +54,8 @@ class CardsController < ApplicationController
   end
 
   def destroy
+    authorize @card
+
     @card.destroy
     redirect_to collection_cards_path(@collection), notice: 'Carta removida com sucesso!'
   end
