30051152dc5a43081cc1c2f3a37d6c8ccce0248b
kesleymartins <kesley.martins@vivaldi.net>
2023-03-13 21:49:54 -0300

    add tests to validate language attribute presence

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Collection, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:privacy) }
+    it { should validate_presence_of(:language) }
   end
 
   context "associations" do
